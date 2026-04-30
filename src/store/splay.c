a0d8b55d2e2922d5131cbecf463fb339505f8ee0
Anton <singaraiona@gmail.com>
2026-04-30 14:43:50 +0200

    feat(store): support string columns in splayed tables

--- src/store/splay.c
+++ src/store/splay.c
@@ -184,6 +184,11 @@ static ray_t* splay_load_impl(const char* dir, const char* sym_path, bool use_mm
         }
 
         ray_t* col = use_mmap ? ray_col_mmap(path) : ray_col_load(path);
+        if (use_mmap && col && RAY_IS_ERR(col) &&
+            strcmp(ray_err_code(col), "nyi") == 0) {
+            ray_release(col);
+            col = ray_col_load(path);
+        }
         if (!col || RAY_IS_ERR(col)) {
             ray_release(schema);
             ray_release(tbl);
