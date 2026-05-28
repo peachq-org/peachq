261fe3d2b2565fea3be1dc8f00f81eac7221a34f
Anton <singaraiona@gmail.com>
2026-05-28 09:21:19 +0200

    refactor(lang): return becomes variadic (0 or 1 arg)

--- src/lang/internal.h
+++ src/lang/internal.h
@@ -454,7 +454,7 @@ ray_t* ray_os_size_fn(ray_t* x);
 ray_t* ray_os_list_fn(ray_t* x);
 ray_t* ray_setenv_fn(ray_t* name, ray_t* val);
 ray_t* ray_quote_fn(ray_t** args, int64_t n);
-ray_t* ray_return_fn(ray_t* x);
+ray_t* ray_return_fn(ray_t** args, int64_t n);
 ray_t* ray_args_fn(ray_t* x);
 ray_t* ray_rc_fn(ray_t* x);
 ray_t* ray_diverse_fn(ray_t* x);
